f21aa35d842baea9aa4ca87c46b077ee1fdeea11
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-10 20:52:05 -0300

    Analisando Bugs no Modo DEBUG
    Analisando Bugs no Modo DEBUG

---
+++ Estudos/AnaliseDeBug.py
@@ -0,0 +1,21 @@
+#Bug 
+print('ola')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 'vinte reais')
+print('programa finalizado')
+
+
+#Após analisar usando o modo DEBUG
+print('ola')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+print('programa finalizado')
+
