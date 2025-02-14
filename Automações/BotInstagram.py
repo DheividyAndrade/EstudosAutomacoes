39486c001c012eb7acaef8780a5bc0276267e1ed
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-13 22:16:33 -0300

    Projeto BOT instagram e curtida 24h
    Projeto BOT instagram e curtida 24h

---
+++ Automações/BotInstagram.py
@@ -0,0 +1,23 @@
+import pyautogui
+from time import sleep
+
+
+
+# Abrir site instagram 'https://www.instagram.com/'
+# Clicar e digitar login/senha
+# Digitar Login
+# Precionar 'tab'
+# Digitar Senha
+# Clicar em Entrar
+# Clicar em 'Agora não'
+# Clicar em pesquisa
+# Escrever Perfil que deseja 'castroraa_'
+# Clicar na POST mais recente
+# Verificar se já foi Curtida( Se já pausar por 24h) Logout
+# Se não, Curti 
+# Clicar no balão de comentario 
+# Clicar na barra de comentario 
+# Escrever Comentario
+# precionar 'ENTER'
+# logout
+# Pausar por 24h
