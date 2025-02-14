d9fbee78038d912effaeb36c0fdf1fabbb7cc556
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-14 20:30:50 -0300

    test projeto 1
    test projeto 1

--- Automações/Automação10.py
+++ Automações/Automação10.py
@@ -2,5 +2,7 @@
 import pyautogui
 
 # quebrando 'não sou um robô'
-cap = pyautogui.locateCenterOnScreen('google3.png')
-pyautogui.click(cap [0], cap[1], duration=2)
+#cap = pyautogui.locateCenterOnScreen('google3.png')
+#pyautogui.click(cap [0], cap[1], duration=2)
+
+print(pyautogui.KEYBOARD_KEYS) 
