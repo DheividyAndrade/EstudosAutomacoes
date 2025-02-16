f20a9e4e78c55b406a91284118ac88521f985284
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-16 13:37:44 -0300

    Projeto Envio Wpp em Massa
    Projeto Envio Wpp em Massa

--- Automações/BOTenviomassawpp.py
+++ Automações/BOTenviomassawpp.py
@@ -16,13 +16,28 @@ import pyautogui
 from time import sleep
 import pyperclip
 
-telefones = [5564992193649, 5582991681996]
+mensagem = "Olá, Gostaria de conhecer mais sobre nossos BOTS e Automações de Sistemas? (Digite sim, se gostaria de participar.)"
 
+# Forma simples de enviar para contatos em massa 
+# telefones = [5564992193649, 5582991681996]
+
+# enviando para contatos em massa atraves de um arquivo
+telefones = []
+
+# forma com arquivo
+with open('fones.txt' ,'r') as arquivo:
+    for linha in arquivo:
+        telefones.append(linha.split('\n')[0]) 
+    print(telefones)
+
+# Loop
 for telefone in telefones:
     webbrowser.open(f'https://api.whatsapp.com/send?phone={telefone}')
     sleep(10)
-    pyautogui.typewrite(
-        'Olá, Gostaria de conhecer mais sobre nossos BOTS e Automações de Sistemas? (Digite sim, se gostaria de participar.)')
+    pyperclip.copy(mensagem)  # Copia a mensagem para a área de transferência
+    pyautogui.hotkey("ctrl", "v")  # Cola a mensagem no chat
     sleep(5)
     pyautogui.press('enter')
     sleep(300)
+
+# Fim
