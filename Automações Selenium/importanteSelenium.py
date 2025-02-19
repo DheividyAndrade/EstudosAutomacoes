6fa9d2ae6af0f33f76161ba55c138c0110c0c94a
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-18 22:22:06 -0300

    Update importal selenium
    Update importal selenium

--- Automações Selenium/importanteSelenium.py
+++ Automações Selenium/importanteSelenium.py
@@ -10,7 +10,7 @@ print(driver.current_url)  # Obtem URL(endereço) da página atual
 print(driver.page_source)  # Obtem o código fonte da página atual
 # obtem o texto dentro de um elemento
 print(driver.find_element(By.XPATH, '//a[@class="navbar-brand"]').text)
-print(driver.find_element(    By.XPATH, '//a[@class="navbar-brand"]').get_attribute("style"))
+print(driver.find_element(By.XPATH, '//a[@class="navbar-brand"]').get_attribute("style"))
 
 driver.close() # Fecha janela atual
 input('')
