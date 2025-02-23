73e8b77540a0d1afac642df2a9d82cdf841097b8
Dheividy Andrade <deividyandrade@hotmail.com>
2025-02-23 11:11:55 -0300

    Desafio/Mudando Janelas no Selenium
    Desafio/Mudando Janelas no Selenium

---
+++ Automações Selenium/Desafio.py
@@ -0,0 +1,94 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.keys import Keys
+
+
+def iniciar_driver():
+    chrome_options = Options()
+    '''
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-US , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
+    arguments = ['--lang=pt-BR', '--window-size=1400,750', '--incognito']
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/master/chrome/common/pref_names.cc
+    # Uso de configurações experimentais
+    chrome_options.add_experimental_option('prefs', {
+        # Alterar o local padrão de download de arquivos
+        'download.default_directory': 'D:\\Storage\\Desktop\\projetos selenium\\downloads',
+        # notificar o google chrome sobre essa alteração
+        'download.directory_upgrade': True,
+        # Desabilitar a confirmação de download
+        'download.prompt_for_download': False,
+        # Desabilitar notificações
+        'profile.default_content_setting_values.notifications': 2,
+        # Permitir multiplos downloads
+        'profile.default_content_setting_values.automatic_downloads': 1,
+
+    })
+    # inicializando o webdriver
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+# 1 - Navegar ate o site
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+sleep(1)
+# Usado para deixar tela completa do navegador.
+driver.maximize_window()
+sleep(2)
+
+#janela inicial
+janela_inicial = driver.current_window_handle
+
+# Clicar em desafio
+botao_desafio = driver.find_element(By.LINK_TEXT, 'Desafios')
+botao_desafio.click()
+sleep(2)
+# Scroll para baixo
+driver.execute_script("window.scrollTo(0, 2500);")
+sleep(3)
+
+botao_abrinova_janela = driver.find_element(By.XPATH, "//button[@onclick='abrirJanelaDesafio()']")
+sleep(1)
+botao_abrinova_janela.click()
+sleep(2)
+
+# verificando quais janelas estão abertas
+janelas = driver.window_handles
+for janela in janelas:
+    print(janela)
+    if janela not in janela_inicial:
+        # alterar para essa nova janela
+        driver.switch_to.window(janela)
+        sleep(2)
+        campo_pesquisa = driver.find_element(By.ID, "opiniao_sobre_curso")
+        sleep(2)
+        campo_pesquisa.send_keys('Estou gostando do curso')
+        sleep(2)
+        botao_pesquisar = driver.find_element(By.ID, "fazer_pesquisa")
+        sleep(1)
+        botao_pesquisar.click()
+        sleep(2)
+# auternando para janela inicial
+driver.switch_to.window(janela_inicial)
+sleep(3)
+# campo janela principal digitar
+campo_escrever_princial = driver.find_element(By.ID,'campo_desafio7')
+campo_escrever_princial.send_keys('Finalizei')
+sleep(3)
+
+
+
+input('')
+driver.close()
+
